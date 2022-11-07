39f4e5bc5be6ca2f06df938d152f954d115d81be
Thinh <a5tt3linhdam@gmail.com>
2022-11-06 22:41:52 -0500

    first commit

---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "../style";
+import { arrowUp } from "../assets";
+
+const GetStarted = () => (
+  <div className={`${styles.flexCenter} w-36 h-36 rounded-full bg-blue-gradient p-0.5 cursor-pointer`}>
+    
+    <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+
+      <div className={`${styles.flexStart} flex-row`}>
+        <p className="font-poppins font-medium text-lg leading-6 mr-2">
+          <span className="text-gradient">Get</span>          
+        </p>
+        <img src={arrowUp} alt="arrow" className="w-6 h-6 object-contain" />        
+      </div>
+
+      <p className="font-poppins font-medium text-lg leading-6">
+        <span className="text-gradient">Started</span>
+      </p>
+
+    </div>
+
+  </div>
+)
+
+
+export default GetStarted;
